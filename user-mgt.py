359c99609874c33fcc3b5f870bf502c5409a7ceb
root <root@DESKTOP-3GRKFDH.>
2024-09-25 00:28:13 +0530

    Added user managment function in user.mgt.py

---
+++ user-mgt.py
@@ -0,0 +1,11 @@
+  def add user(username):
+
+      print(f"User {username} added successfully!")
+
+      def delete user(username):
+
+          print(f"User {username} deleted successfully!")
+
+          add_user("john_doe")
+
+          delete user("john_doe")def add user(username):
