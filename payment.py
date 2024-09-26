3f7daccb57da6ecb52bb55d5e387227c5dc594a0
“[firstname <root@DESKTOP-3GRKFDH.>
2024-09-27 00:33:30 +0530

    cheking now from new branch

--- payment.py
+++ payment.py
@@ -1,4 +1,6 @@
     This is the file for git to use 
 
     trying to prevent Develope branch so no one can pust code directly.
+    
+    Now update the file, needs to check now whether it can  push or not.
 
